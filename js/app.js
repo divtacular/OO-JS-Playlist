9cba4e1671fc2e4de80cdfeca79bdb098c048c5b
David <me@davidsmythe.com>
2015-07-20 21:57:35 +0100

    Chapter 1 complete
    Playlist cycles through given songs and updates UI components.

---
+++ js/app.js
@@ -0,0 +1,30 @@
+var playlist = new Playlist();
+
+var songOne = new Song("Song One", "Artist One", "3:00");
+var songTwo = new Song("Song Two", "Artist Two", "3:30");
+
+playlist.add(songOne);
+playlist.add(songTwo);
+
+var playlistElement = document.getElementById("playlist");
+
+playlist.renderInElement(playlistElement);
+
+var playButton = document.getElementById("play");
+playButton.onclick = function(){
+    playlist.play();
+    playlist.renderInElement(playlistElement);
+};
+
+var nextButton = document.getElementById("next");
+nextButton.onclick = function(){
+    playlist.next();
+    playlist.renderInElement(playlistElement);
+};
+
+var stopButton = document.getElementById("stop");
+stopButton.onclick = function(){
+    playlist.stop();
+    playlist.renderInElement(playlistElement);
+};
+
